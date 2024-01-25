2d681eec9abb89f326500aadaeab43c7a5b3ea04
Divyesh Parmar <127118175+Divyesh032040@users.noreply.github.com>
2024-01-25 12:14:12 +0530

    Add files via upload

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+
+        <p>
+          Congratulation Divyesh for your first React App 
+        </p>
+
+    </div>
+  );
+}
+
+export default App;
